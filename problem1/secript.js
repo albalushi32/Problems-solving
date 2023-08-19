2579cd83ad390def9372e919158abd442cb3f0b5
fatma <141551725+albalushi32@users.noreply.github.com>
2023-08-19 15:53:45 +0400

    Add files via upload

---
+++ problem1/secript.js
@@ -0,0 +1,17 @@
+var n1;
+var n2;
+var n3;
+if(n1> n2 && n1>n3){
+    console.log(`number 1 is greter= ${n1}`)
+
+}
+else if(n2>n1 && n2 > n3){
+    console.log(`number 2 is the greter =${n2}`)
+}
+else if (n3 >n1 && n3 > n2){
+    console.log(`number 3 is the greter = ${n3}`)
+}
+else {
+      console.log(" Thank you")
+  }
+
